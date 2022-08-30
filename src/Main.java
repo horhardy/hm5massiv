c804de66b12aa879246e357fd98b2aa31133100b
horhardy <horhardy@mail.ru>
2022-08-31 01:40:53 +0500

    исправил 2 раз дз 5 массив

--- src/Main.java
+++ src/Main.java
@@ -35,36 +35,35 @@ public class Main {
         points[0] = 1;
         points[1] = 2;
         points[2] = 3;
-        if (points.length>=1) {
-            System.out.print(points[0]); }
-            for (int i = 1; i<points.length; i++)
-                System.out.print("," + points[i]);
+
+            for (int i = 0; i<points.length; i++) {
+                System.out.print(points[i]);
+                if (i<2)
+                    System.out.print(", ");
+            }
 
 
-
-
-
-
-        System.out.println();
+            System.out.println();
 
         // ЗАДАНИЕ 2.2
 
         float[] weightA = {1.57f, 7.654f, 9.986f};
 
-        if ( weightA.length>=1)
-            System.out.print( weightA[0]);
-            for (int i = 1; i< weightA.length; i++)
-                System.out.print("," +  weightA[i]);
+        for (int i = 0; i< weightA.length; i++) {
+                System.out.print(weightA[i]);
+            if (i<2)
+                System.out.print(", ");
+        }
 
         System.out.println();
 
         // ЗАДАНИЕ 2.3
 
         int[] yearA = {2000, 2001, 2002, 2003, 2004};
-        if (yearA.length>=1 )
-            System.out.print(yearA[0]);
-        for (int i = 1; i < yearA.length; i++)
-            System.out.print("," + yearA[i] );
+        for (int i = 0; i < yearA.length; i++) {
+            System.out.print( yearA[i] );
+        if (i<4)
+            System.out.print(", "); }
 
 
 
@@ -76,10 +75,11 @@ public class Main {
         pointsA[0] = 1;
         pointsA[1] = 2;
         pointsA[2] = 3;
-        if (  pointsA.length>=1)
-            System.out.print(  pointsA[2]);
-        for (int i =  pointsA.length - 2; i >= 0; i--)
-            System.out.print("," +   pointsA[i]);
+
+        for (int i =  pointsA.length - 1; i >= 0; i--) {
+            System.out.print( pointsA[i]);
+        if (i > 0)
+            System.out.print(", "); }
 
 
 
@@ -90,10 +90,10 @@ public class Main {
         float[] weightB = {1.57f, 7.654f, 9.986f};
 
 
-        if ( weightB.length>=1)
-            System.out.print( weightB[2]);
-        for (int i = weightB.length - 2; i >= 0; i--)
-            System.out.print("," +  weightB[i]);
+        for (int i = weightB.length - 1; i >= 0; i--)  {
+            System.out.print(weightB[i]);
+        if (i > 0)
+            System.out.print(", "); }
 
 
 
@@ -103,11 +103,12 @@ public class Main {
         // ЗАДАНИЕ 3.3
 
         int[] yearB = {2000, 2001, 2002, 2003, 2004};
-        if (yearB.length>=1 )
-            System.out.print(yearB[4]);
-        for (int i = yearB.length - 2; i >= 0; i--)
-           // System.out.print(yearB[i] + ",");
-            System.out.print("," + yearB[i] );
+        for (int i = yearB.length -1; i >= 0; i--) {
+            System.out.print(yearB[i]);
+            if (i > 0)
+                System.out.print(", ");
+            }
+
 
 
 
@@ -116,19 +117,14 @@ public class Main {
 
 
         // ЗАДАНИЕ 4
-        int[] weightsV = new int[3];
-        weightsV[0] = 1;
-        weightsV[1] = 2;
-        weightsV[2] = 3;
-        int pointOneV = weightsV[0];
-        for (; pointOneV % 2 != 0; pointOneV++)
-            System.out.println(pointOneV);
-        int pointTwoV = weightsV[1];
-        for (; pointTwoV % 2 != 0; pointTwoV++)
-                System.out.println(pointTwoV);
-        int pointThreeV = weightsV[2];
-        for (; pointThreeV % 2 != 0; pointThreeV++)
-                System.out.println(pointThreeV);
+        int[] weightsV =  {1, 2, 3};
+
+        for (int i = 0; i<weightsV.length; i++) {
+            if (weightsV[i] % 2 != 0  )
+                weightsV[i] = weightsV[i] + 1;
+
+            System.out.println(weightsV[i]);}
+
 
 
     }
